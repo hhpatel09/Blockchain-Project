04cfb1db665f9b19f70fdf6586345afff13f7f7a
Harsh Patel <hhpatel09@gmail.com>
2019-12-07 22:45:45 -0600

    figuring out GUI

--- tk.py
+++ tk.py
@@ -5,16 +5,17 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.geometry("600x600")
+        # self.master.resizable(0, 0)
         self.pack()
         self.create_widgets()
         self.number = 0
 
     def create_widgets(self):
+        self.winfo_toplevel().title("Doge Explorer")
         self.hi_there = tk.Button(self)
-        # self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["text"] = 0
         self.hi_there["command"] = self.say_hi
-        # self.hi_there["yeet"] = self.say_hi
         self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
@@ -24,7 +25,6 @@ class Application(tk.Frame):
     def say_hi(self):
         self.hi_there["text"] = self.number
         self.number = self.number + 1
-        # print("hi there, everyone!")
 
 
 root = tk.Tk()

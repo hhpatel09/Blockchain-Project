bb8053be91f0ccc6d38bada8a5b90a7cad2354e3
coopbell <cooper-bell@uiowa.edu>
2019-12-07 19:56:41 -0600

    removing venv2, adding it to gitignore

--- ping.py
+++ ping.py
@@ -6,19 +6,46 @@
 import asyncio
 import websockets
 import json
+import sys
+
+apiMsgs = []
+done = 0
 
 
-async def pingApi():
+async def PingApi(msgToSend):
+    # uri = "ws://ws.dogechain.info/inv"
     uri = "wss://ws.blockchain.info/inv"
     async with websockets.connect(uri) as websocket:
-        msg = '{"op":"ping_block"}'
-        await websocket.send(msg)
+        await websocket.send(msgToSend)
+        while not done:
+            result = await websocket.recv()
+            # print(f"< {result}")
+            apiMsgs.append(result)
 
-        result = await websocket.recv()
-        # print(f"< {result}")
-        return result
 
-while True:
-    returnMsg = asyncio.run(pingApi())
-    parsedMsg = json.loads(returnMsg)
-    print(parsedMsg["x"])
+async def getElementFromList():
+    if apiMsgs:
+        msg = apiMsgs[1]
+        apiMsgs.remove(msg)
+
+
+async def chain(msgToSend):
+    apiMsgs = await PingApi(msgToSend)
+    msg = await getElementFromList()
+    print("hello")
+
+
+assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+msgToSend = '{"op":"unconfirmed_sub"}'
+asyncio.run(chain(msgToSend))
+# returnMsg = asyncio.run(PingApi(msgToSend))
+# for i in range(0, 10):
+#     if apiMsgs:
+#         msg = apiMsgs[1]
+#         apiMsgs.remove(msg)
+#         parsedMsg = json.loads(msg)
+#         print(parsedMsg["msg"])
+
+msgToSend = '{"op":"unconfirmed_unsub"}'
+returnMsg = asyncio.run(PingApi(msgToSend))
+done = 1
